40ad67bf38a27a1de85c6bc15e6ce70b99465bdc
Sebastián Peyrott <sebastian.peyrott@auth0.com>
2016-10-27 05:35:30 -0300

    Update all libraries. Change contact list.

--- server/server.js
+++ server/server.js
@@ -9,39 +9,39 @@ app.use(cors());
 // This middleware will check incoming requests for a valid
 // JWT on any routes that it is applied to.
 const authCheck = jwt({
-  secret: new Buffer('YOUR_AUTH0_SECRET', 'base64'),
-  audience: 'YOUR_AUTH0_CLIENT_ID'
+  secret: new Buffer('AUTH0_CLIENT_SECRET', 'base64'),
+  audience: 'AUTH0_CLIENT_ID'
 });
 
 var contacts = [
   {
     id: 1,
-    name: 'Chris Sevilleja',
-    email: 'chris@scotch.io',
-    image: '//gravatar.com/avatar/8a8bf3a2c952984defbd6bb48304b38e?s=200'
+    name: 'Kim',
+    email: 'kim@email.com',
+    image: 'https://en.gravatar.com/userimage/20807150/4c9e5bd34750ec1dcedd71cb40b4a9ba.png'
   },
   {
     id: 2,
-    name: 'Nick Cerminara',
-    email: 'nick@scotch.io',
-    image: '//gravatar.com/avatar/5d0008252214234c609144ff3adf62cf?s=200'
+    name: 'Gonto',
+    email: 'gonto@email.com',
+    image: 'https://www.gravatar.com/avatar/df6c864847fba9687d962cb80b482764??s=200'
   },
   {
     id: 3,
-    name: 'Ado Kukic',
-    email: 'ado@scotch.io',
+    name: 'Ado',
+    email: 'ado@email.com',
     image: '//gravatar.com/avatar/99c4080f412ccf46b9b564db7f482907?s=200'
   },
   {
     id: 4,
-    name: 'Holly Lloyd',
-    email: 'holly@scotch.io',
-    image: '//gravatar.com/avatar/5e074956ee8ba1fea26e30d28c190495?s=200'
+    name: 'Sebastián',
+    email: 'sebastian@email.com',
+    image: 'http://en.gravatar.com/userimage/92476393/001c9ddc5ceb9829b6aaf24f5d28502a.png?size=200'
   },
   {
     id: 5,
-    name: 'Ryan Chenkie',
-    email: 'ryan@scotch.io',
+    name: 'Ryan',
+    email: 'ryan@email.com',
     image: '//gravatar.com/avatar/7f4ec37467f2f7db6fffc7b4d2cc8dc2?s=200'
   }
 ];
@@ -54,7 +54,7 @@ app.get('/api/contacts', (req, res) => {
 });
 
 app.get('/api/contacts/:id', authCheck, (req, res) => {
-  res.json(contacts.filter(contact => contact.id === parseInt(req.params.id)));
+  res.json(contacts.filter(contact => contact.id === parseInt(req.params.id))[0]);
 });
 
 app.listen(3001);
--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 class AppComponent extends Component {
   
   componentWillMount() {
-    this.lock = new Auth0Lock('YOUR_AUTH0_CLIENT_ID', 'YOUR_AUTH0_DOMAIN');
+    this.lock = new Auth0Lock('AUTH0_CLIENT_ID', 'AUTH0_CLIENT_DOMAIN');
   }
   
   render() {
